e314093535a3d51d91b4edba048c579b10a767ce
eduard <eduard.babayan@cognaize.com>
2024-06-24 17:18:10 +0400

    ADDED path to training and validating files and configurations for dataloader such as batch size and number of workers

--- src/config.py
+++ src/config.py
@@ -4,3 +4,10 @@ from pathlib import Path
 class CFG:
     root = Path(__file__).parent.parent.absolute()
     save_path = root.joinpath('processed')
+
+    training_data_dir = save_path.joinpath('train')
+    validation_data_dir = save_path.joinpath('val')
+
+    batch_size = 16
+    num_workers = 4
+
